61e3ac8d2062fa28b91ea88ae9012735de39b738
nysascape <jago@nysascape.digital>
2020-05-18 06:35:15 +0900

    Revert "Add 'sepolicy/' from commit 'b9a80fd7d8aec5b9c18ec8b2a42035d8ddedce0f'"
    This reverts commit de4a7fe32267739e7783e737a8ce51886c63a514.

--- sepolicy/bonito-sepolicy.mk
+++
@@ -1,8 +0,0 @@
-PRODUCT_PUBLIC_SEPOLICY_DIRS := device/google/bonito-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS := device/google/bonito-sepolicy/private
-
-# vendors
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/sdm710
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/verizon
